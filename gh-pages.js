92a7eb53c1a49a4010966fb82e25335dc10b8d8b
wellfrog <wellfrog16@gmail.com>
2022-10-18 23:02:23 +0800

    更新框架

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ const ghpages = require('gh-pages')
 ghpages.publish('docs/.vuepress/dist', {
     remote: 'gitee',
     message: 'Updates',
-    repo: 'https://gitee.com/wellfrog16/element-plus-components.git',
+    repo: 'https://gitee.com/wellfrog16/vc.git',
 }, () => {
     console.log('finished')
 })

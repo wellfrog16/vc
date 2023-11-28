91d9b38b49b3944fac1f3b5e1daa0b3182dc6938
wellfrog <wellfrog16@gmail.com>
2023-11-28 10:44:11 +0800

    build: 📦️ 增加指令的构建

--- build/esbuild.ts
+++ build/esbuild.ts
@@ -11,7 +11,7 @@ const componentEntrys = klawSync(COMPONENTS_PATH, {
     depthLimit: 0,
 }).map(dir => `${dir.path}/index.vue`)
 
-async function run(options?: BuildOptions) {
+async function componentRun(options?: BuildOptions) {
     await build({
         outdir: './es/components',
         outExtension: { '.js': '.mjs' },
@@ -29,4 +29,32 @@ async function run(options?: BuildOptions) {
     })
 }
 
-run()
+componentRun()
+
+const DIRECTIVES_PATH = path.resolve(__dirname, '../src/directives')
+
+const directiveEntrys = klawSync(DIRECTIVES_PATH, {
+    nofile: true,
+    depthLimit: 0,
+}).map(dir => `${dir.path}/index.ts`)
+directiveEntrys.push(`${DIRECTIVES_PATH}/index.ts`)
+
+async function directiveRun(options?: BuildOptions) {
+    await build({
+        outdir: './es/directives',
+        outExtension: { '.js': '.mjs' },
+        bundle: true,
+        entryPoints: directiveEntrys,
+        sourcemap: false,
+        plugins: [
+            pluginVue(),
+        ],
+        // loader: { '.png': 'dataurl' },
+        external: ['vue', 'element-plus', 'lodash-es', '@element-plus/icons-vue', 'vuedraggable', '@wfrog/utils'],
+        format: 'esm',
+        minify: false,
+        ...options,
+    })
+}
+
+directiveRun()

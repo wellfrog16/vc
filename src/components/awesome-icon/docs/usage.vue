09b75e0f5fb9a29cbdbf1eabeca2e355cc6619c9
wellfrog <wellfrog16@gmail.com>
2024-10-24 13:43:50 +0800

    docs: :memo: 更新文档

--- src/components/awesome-icon/docs/usage.vue
+++ src/components/awesome-icon/docs/usage.vue
@@ -7,7 +7,7 @@ import Document from '@/components/document.vue'
 
 const documentData = {
     params: [
-        { param: '*name', description: 'icon 对应的 name，详情点击后面的链接', type: 'https://fontawesome.com/search?m=free&s=brands%2Csolid%2Cregular', defaultValue: '-' },
+        { param: '*name', description: 'icon 对应的 name，详情点击后面的链接', type: 'https://fontawesome.com/search?o=r&m=free', defaultValue: '-' },
         { param: 'mode', description: '使用 Css 还是 SVG', type: 'css | svg', defaultValue: 'css' },
     ],
 }

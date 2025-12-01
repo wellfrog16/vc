3342d4d8e70210e0d165b3f1be0d4bcc1f28d108
wellfrog <wellfrog16@gmail.com>
2025-12-01 16:32:42 +0800

    docs: :memo: 完善 awesome-icon 的 playground

--- playground/src/views/components/components/awesome-icon.vue
+++ playground/src/views/components/components/awesome-icon.vue
@@ -1,11 +1,15 @@
 <template>
-    <el-space>
-        <HAwesomeIcon name="fa-brands fa-bilibili" />
-        <HAwesomeIcon name="fa-solid fa-car-side" size="30px" />
-        <HAwesomeIcon name="fa-solid fa-chart-line" size="40" color="yellowgreen" />
-        <HAwesomeIcon name="fa-brands fa-github-alt" size="30" color="#e89f40" />
-        <HAwesomeIcon name="fa-regular fa-face-laugh-beam" size="1.5em" />
-        <HAwesomeIcon name="fa-regular fa-id-card" color="red" />
+    <el-space direction="vertical" fill size="large" :style="{ display: 'flex' }">
+        <el-alert title="icon 如果没显示，可能需要加强网络" type="warning" :closable="false" />
+
+        <el-space>
+            <HAwesomeIcon name="fa-brands fa-bilibili" />
+            <HAwesomeIcon name="fa-solid fa-car-side" size="30px" />
+            <HAwesomeIcon name="fa-solid fa-chart-line" size="40" color="yellowgreen" />
+            <HAwesomeIcon name="fa-brands fa-github-alt" size="30" color="#e89f40" />
+            <HAwesomeIcon name="fa-regular fa-face-laugh-beam" size="1.5em" />
+            <HAwesomeIcon name="fa-regular fa-id-card" color="red" />
+        </el-space>
     </el-space>
 </template>
 

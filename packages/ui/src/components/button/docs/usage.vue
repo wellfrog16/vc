86e38801428f2d41471df4d8f10261a926593e60
wellfrog <wellfrog16@gmail.com>
2025-12-01 19:06:54 +0800

    docs: :memo: 修复文档的外链

--- packages/ui/src/components/button/docs/usage.vue
+++ packages/ui/src/components/button/docs/usage.vue
@@ -11,7 +11,7 @@ const documentData = {
         { param: 'confirm', description: '是否显示二次确认框，设置 none 则不二次确认', type: 'IConfirmInfo', defaultValue: '下一行' },
         { param: '-', rowType: 'defaultValue', description: '{ type: \'messagebox\', title: \'提示\', confirmButtonText: \'确定\', cancelButtonText: \'取消\', msg: \'确认要删除这条数据吗\' }' },
         { param: '*icon', description: 'icon 的设置信息', type: 'IIconOption', defaultValue: '-' },
-        { param: '其他', description: 'el-button 的属性、事件', type: 'https://element-plus.gitee.io/zh-CN/component/button.html#button-%E5%B1%9E%E6%80%A7', defaultValue: '-' },
+        { param: '其他', description: 'el-button 的属性、事件', type: 'https://element-plus.org/zh-CN/component/button#button-api', defaultValue: '-' },
     ],
     types: [
         { param: 'IIconOption', description: '{ type: el | svg | awesome, name: string, position: left | right }' },

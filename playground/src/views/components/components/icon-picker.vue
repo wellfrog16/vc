bb3a6e79653acc3683a9c1e8045d37e9da349469
wellfrog <wellfrog16@gmail.com>
2026-01-20 15:00:35 +0800

    feat: ✨ playground 调试增加 scrollbar 和 icon-picker

---
+++ playground/src/views/components/components/icon-picker.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-space size="large" direction="vertical" alignment="left">
+        <vc-icon-picker v-model="result" />
+        <el-text>
+            结果：
+            <el-text type="primary"><vc-iconify-icon :name="result" />{{ result }}</el-text>
+        </el-text>
+    </el-space>
+</template>
+
+<script setup lang="ts">
+const result = ref('')
+</script>
---
+++ playground/src/views/components/components/scrollbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :class="$style.wrapper">
+        <div :class="$style.container">
+            <VcScrollbar flex>
+                <div v-for="i in 20" :key="i">{{ i }}</div>
+            </VcScrollbar>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" module>
+.wrapper {
+    display: flex;
+    height: 200px;
+    flex-direction: column;
+}
+
+.container {
+    display: flex;
+    height: 10px;
+    flex-direction: column;
+    flex-grow: 1;
+}
+</style>

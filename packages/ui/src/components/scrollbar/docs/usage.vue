6fdd5d9b372be90235a543b8d551e1ac1cf94024
wellfrog <wellfrog16@gmail.com>
2026-02-03 14:27:15 +0800

    docs: :memo: 更新 scrollbar 文档

--- packages/ui/src/components/scrollbar/docs/usage.vue
+++ packages/ui/src/components/scrollbar/docs/usage.vue
@@ -6,6 +6,7 @@
 const documentData = {
     params: [
         { param: 'flex', description: '修改成 flex 布局', type: 'boolean', defaultValue: 'false' },
+        { param: 'padding', description: '设置内边距', type: 'number', defaultValue: '0' },
         { param: '其他', description: 'el-scrollbar 的属性、事件', type: 'https://element-plus.org/zh-CN/component/scrollbar#api' },
     ],
 }
